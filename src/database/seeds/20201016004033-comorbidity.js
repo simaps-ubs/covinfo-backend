e4d7538d2ccd51af6655790996640ecfcd6058b2
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2020-11-05 00:12:18 -0300

    Modificando comorbidades

--- src/database/seeds/20201016004033-comorbidity.js
+++ src/database/seeds/20201016004033-comorbidity.js
@@ -1,22 +1,63 @@
 module.exports = {
   up: async (QueryInterface) => {
     await QueryInterface.bulkInsert(
-      'comorbidity',
+      'comorbidities',
       [
         {
-          comorbidity_description: 'Fumante',
+          comorbidity_description: 'Doença no pulmão',
+          question: 'Possui doença no pulmão?',
         },
         {
           comorbidity_description: 'Doença no coração',
+          question: 'Possui doença no coração?',
         },
         {
-          comorbidity_description: 'Doença no pulmão',
+          comorbidity_description: 'Doença nos rins',
+          question: 'Possui doença nos rins?',
+        },
+        {
+          comorbidity_description: 'Problema de saúde mental',
+          question: 'Possui algum problema de saúde mental?',
+        },
+        {
+          comorbidity_description: 'Hipertensão',
+          question: 'Possui hipertensão arterial?',
+        },
+        {
+          comorbidity_description: 'AVC/Derrame',
+          question: 'Já sofreu um AVC/Derrma?',
+        },
+        {
+          comorbidity_description: 'Infarto',
+          question: 'Já sofreu um infarto?',
+        },
+        {
+          comorbidity_description: 'AVC/Derrame',
+          question: 'Já sofreu um AVC/Derrame?',
+        },
+        {
+          comorbidity_description: 'Câncer',
+          question: 'Tem ou teve câncer?',
+        },
+        {
+          comorbidity_description: 'Hanseníase',
+          question: 'Está com hanseníase?',
+        },
+        {
+          comorbidity_description: 'Tuberculose',
+          question: 'Está com tuberculose?',
         },
         {
           comorbidity_description: 'Diabetes',
+          question: 'Tem diabetes?',
         },
         {
-          comorbidity_description: 'Hipertensão arterial',
+          comorbidity_description: 'Álcool',
+          question: 'Faz uso de Álcool?',
+        },
+        {
+          comorbidity_description: 'Acamada',
+          question: 'Está acamada?',
         },
       ],
       {}
