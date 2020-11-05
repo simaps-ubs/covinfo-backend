535f37eb226b13f802d5461a777e27ae9da5dfae
matheus456 <matheus456456@gmail.com>
2020-11-05 18:31:20 -0300

    Adiciona cadastro de dependentes e atualiza métodos da controller de forms
    Signed-off-by: matheus456 <matheus456456@gmail.com>
    Co-authored-by: byronkamal <byronkamal@hotmail.com>
    Co-authored-by: IgorVeludo <velvet.guimaraes@gmail.com>

--- src/database/seeds/20201016004033-comorbidity.js
+++ src/database/seeds/20201016004033-comorbidity.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: async (QueryInterface) => {
     await QueryInterface.bulkInsert(
-      'comorbidity',
+      'comorbidities',
       [
         {
           comorbidity_description: 'Fumante',
