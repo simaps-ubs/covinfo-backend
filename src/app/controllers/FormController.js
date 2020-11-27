02d7c2e8d050dd17ce49240c9a4a7c88c492b2a0
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2020-11-26 23:53:55 -0300

    Modificando rotas GET para route params

--- src/app/controllers/FormController.js
+++ src/app/controllers/FormController.js
@@ -52,15 +52,15 @@ class FormController {
     });
   }
 
-  async getUserDependentsForm(req, res) {z
+  async getUserDependentsForm(req, res) {
     const getUserDependentsFormService = new GetUserDependentsFormService();
-    const userForm = await getUserDependentsFormService.execute(req.params);
+    const userForm = await getUserDependentsFormService.execute(req.body);
     return res.status(200).json(userForm);
   }
 
   async getUserForm(req, res) {
     const getUserFormService = new GetUserFormService();
-    const userForm = await getUserFormService.execute(req.params);
+    const userForm = await getUserFormService.execute(req.body);
     return res.status(200).json(userForm);
   }
 }
