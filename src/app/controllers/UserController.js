cbfba3f8cab9ececa53912eeb417186a26588657
Paulo Henrique Rosa <paulohenriquerosa04@gmail.com>
2020-11-27 08:39:08 -0300

    Modificando o user por user_type na request

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -54,6 +54,7 @@ class UserController {
       email: Yup.string().email().required(),
     });
 
+
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
