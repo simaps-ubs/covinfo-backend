dbce85d31c3a62c11123ed34f48eb1015367e3c8
Matheus Roberto <as.matheusroberto@gmail.com>
2020-12-07 21:19:10 -0300

    Configurando testes

---
+++ src/app/services/GetUserFormService.spec.js
@@ -0,0 +1,15 @@
+describe('AuthentiacteUser', () => {
+    beforeEach(() => {});
+  
+    it('should be able to authenticate', async () => {
+      expect(1 + 2).toBe(3);
+    });
+  
+    it('should not be able to authenticate with non existing user', async () => {
+      expect(1 - 2).toBe(-1);
+    });
+  
+    it('should not be able to authenticate with wrong password', async () => {
+      expect(1 * 2).toBe(2);
+    });
+});
